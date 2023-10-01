783168c754f938a42f4d4359397544f79705b514
hnfnfl <kakaknaufal@gmail.com>
2023-10-01 07:17:13 +0700

    Initial commit

---
+++ app/src/main/java/com/jaylangkung/bpkpd/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.jaylangkung.bpkpd.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jaylangkung.bpkpd.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
