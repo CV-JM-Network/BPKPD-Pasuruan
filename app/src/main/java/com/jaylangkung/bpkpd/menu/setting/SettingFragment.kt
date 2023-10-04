bbbbc45bcfbe46df5b66d7390a1d192a9030783d
Hanif Naufal Ashari <101613140+hnfNaufal@users.noreply.github.com>
2023-10-04 17:55:54 +0700

    feat: - add logout logic - show user data for update profile

--- app/src/main/java/com/jaylangkung/bpkpd/menu/setting/SettingFragment.kt
+++ app/src/main/java/com/jaylangkung/bpkpd/menu/setting/SettingFragment.kt
@@ -1,14 +1,27 @@
 package com.jaylangkung.bpkpd.menu.setting
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.github.dhaval2404.imagepicker.ImagePicker
+import com.jaylangkung.bpkpd.BuildConfig
+import com.jaylangkung.bpkpd.R
+import com.jaylangkung.bpkpd.auth.LoginActivity
 import com.jaylangkung.bpkpd.databinding.FragmentSettingBinding
 import com.jaylangkung.bpkpd.viewModel.SettingViewModel
 import com.jaylangkung.bpkpd.viewModel.ViewModelFactory
+import dev.shreyaspatil.MaterialDialog.MaterialDialog
+import es.dmoral.toasty.Toasty
 
 class SettingFragment : Fragment() {
     private var _binding: FragmentSettingBinding? = null
@@ -24,7 +37,91 @@ class SettingFragment : Fragment() {
         val factory = ViewModelFactory.getInstance(requireActivity().application)
         viewModel = ViewModelProvider(this, factory)[SettingViewModel::class.java]
 
+        binding.apply {
+            viewModel.userData.observe(viewLifecycleOwner) { userData ->
+                Glide.with(requireContext())
+                    .load(userData.img)
+                    .placeholder(R.drawable.ic_profile)
+                    .error(R.drawable.ic_profile)
+                    .into(imgProfile)
+
+                tvValueNameEdit.setText(userData.nama)
+                tvValueEmailEdit.setText(userData.email)
+                tvValueAddressEdit.setText(userData.alamat)
+                tvValuePhoneEdit.setText(userData.telp)
+            }
+
+            appVersion.text = getString(R.string.app_ver, BuildConfig.VERSION_NAME)
+
+            btnChangeImg.setOnClickListener {
+                ImagePicker.with(requireActivity()).apply {
+                    cropSquare()
+                    compress(1024)
+                    maxResultSize(1080, 1080)
+                    galleryMimeTypes(arrayOf("image/png", "image/jpg", "image/jpeg"))
+
+                    createIntent {
+                        startForProfileImageResult.launch(it)
+                    }
+                }
+            }
+
+            btnSave.setOnClickListener {
+                val idAdmin = viewModel.userData.value?.idadmin.toString()
+                val nama = tvValueNameEdit.text.toString()
+                val email = tvValueEmailEdit.text.toString()
+                val alamat = tvValueAddressEdit.text.toString()
+                val telp = tvValuePhoneEdit.text.toString()
+                val tokenAuth = viewModel.userData.value?.tokenAuth.toString()
+
+                Toasty.info(requireContext(), "Berhasil Update Profile", Toasty.LENGTH_SHORT).show()
+                btnSave.startAnimation()
+            }
+
+            btnLogout.setOnClickListener {
+                val dialog = MaterialDialog.Builder(requireActivity())
+                    .setTitle("Logout")
+                    .setMessage(getString(R.string.confirm_logout))
+                    .setCancelable(true)
+                    .setPositiveButton(getString(R.string.no), R.drawable.ic_close) { dialogInterface, _ ->
+                        dialogInterface.dismiss()
+                    }
+                    .setNegativeButton(getString(R.string.yes), R.drawable.ic_logout) { dialogInterface, _ ->
+                        dialogInterface.dismiss()
+                        viewModel.logout()
+                        Toasty.info(requireContext(), "Berhasil Logout", Toasty.LENGTH_SHORT).show()
+                        activity?.let {
+                            it.startActivity(Intent(it, LoginActivity::class.java))
+                            it.finish()
+                        }
+                    }
+                    .build()
+                dialog.show()
+            }
+        }
+
         return binding.root
+    }
+
+    private val startForProfileImageResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+        val resultCode = result.resultCode
+        val data = result.data
+        when (resultCode) {
+            Activity.RESULT_OK -> {
+                //Image Uri will not be null for RESULT_OK
+                val fileUri = data?.data!!
+                viewModel.photoUri = fileUri
+                binding.imgProfile.setImageURI(fileUri)
+            }
+
+            ImagePicker.RESULT_ERROR -> {
+                Toast.makeText(requireContext(), ImagePicker.getError(data), Toast.LENGTH_SHORT).show()
+            }
+
+            else -> {
+                Log.d("Cancel image picking", "Task Cancelled")
+            }
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/jaylangkung/bpkpd/viewModel/SettingViewModel.kt
+++ app/src/main/java/com/jaylangkung/bpkpd/viewModel/SettingViewModel.kt
@@ -1,8 +1,43 @@
 package com.jaylangkung.bpkpd.viewModel
 
 import android.app.Application
+import android.net.Uri
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.jaylangkung.bpkpd.dataClass.UserData
+import com.jaylangkung.bpkpd.utils.Constants
+import com.jaylangkung.bpkpd.utils.MySharedPreferences
 
 class SettingViewModel(application: Application) : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val appContext: Application = application
+    private lateinit var myPreferences: MySharedPreferences
+    var photoUri: Uri? = null
+
+    val userData: LiveData<UserData> = run {
+        myPreferences = MySharedPreferences(appContext)
+        val tokenAuth = myPreferences.getValue(Constants.TokenAuth).toString()
+        val liveData = MutableLiveData<UserData>()
+        liveData.postValue(
+            UserData(
+                myPreferences.getValue(Constants.USER_ALAMAT).toString(),
+                "",
+                myPreferences.getValue(Constants.USER_EMAIL).toString(),
+                myPreferences.getValue(Constants.USER_IDADMIN).toString().toInt(),
+                myPreferences.getValue(Constants.USER_IDLEVEL).toString().toInt(),
+                myPreferences.getValue(Constants.USER_FOTO).toString(),
+                myPreferences.getValue(Constants.USER_JUDUL).toString(),
+                myPreferences.getValue(Constants.USER_NAMA).toString(),
+                myPreferences.getValue(Constants.USER_TELP).toString(),
+                tokenAuth
+            )
+        )
+        liveData
+    }
+
+
+    fun logout() {
+        myPreferences.clear()
+    }
 }
